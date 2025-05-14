4b88c8bd34ec649cc58a0a28e624187b91ec50d2
milekboy <tijaniwahab25@gmail.com>
2025-05-14 10:02:24 +0100

    remove extra menu path

--- src/app/(pages)/menu/page.jsx
+++ src/app/(pages)/menu/page.jsx
@@ -9,16 +9,16 @@ import PromoSection from "@components/sections/Promo";
 
 export const metadata = {
   title: {
-		default: "Menu",
-	},
+    default: "Menu",
+  },
   description: AppData.settings.siteDescription,
-}
+};
 
 const Menu1 = () => {
   return (
     <>
       <PageBanner pageTitle={"Starbelly menu."} breadTitle={"Menu"} type={1} />
-      
+
       {/* menu section 1 */}
       <section className="sb-menu-section sb-p-90-60">
         <div className="sb-bg-1">
@@ -26,11 +26,18 @@ const Menu1 = () => {
         </div>
         <div className="container">
           <div className="sb-mb-60">
-            <h2 className="sb-cate-title sb-mb-15">{MenuData.categories[0].name}</h2>
-            <p className="sb-text" dangerouslySetInnerHTML={{__html : MenuData.categories[0].description}} />
+            <h2 className="sb-cate-title sb-mb-15">
+              {MenuData.categories[0].name}
+            </h2>
+            <p
+              className="sb-text"
+              dangerouslySetInnerHTML={{
+                __html: MenuData.categories[0].description,
+              }}
+            />
           </div>
 
-          <MenuGrid items={MenuData.categories[0].items} />
+          {/* <MenuGrid items={MenuData.categories[0].items} /> */}
         </div>
       </section>
       {/* menu section 1 end */}
@@ -42,11 +49,18 @@ const Menu1 = () => {
         </div>
         <div className="container">
           <div className="sb-mb-60">
-            <h2 className="sb-cate-title sb-mb-15">{MenuData.categories[1].name}</h2>
-            <p className="sb-text" dangerouslySetInnerHTML={{__html : MenuData.categories[1].description}} />
+            <h2 className="sb-cate-title sb-mb-15">
+              {MenuData.categories[1].name}
+            </h2>
+            <p
+              className="sb-text"
+              dangerouslySetInnerHTML={{
+                __html: MenuData.categories[1].description,
+              }}
+            />
           </div>
 
-          <MenuGrid items={MenuData.categories[1].items} />
+          {/* <MenuGrid items={MenuData.categories[1].items} /> */}
         </div>
       </section>
       {/* menu section 2 end */}
@@ -58,11 +72,18 @@ const Menu1 = () => {
         </div>
         <div className="container">
           <div className="sb-mb-60">
-            <h2 className="sb-cate-title sb-mb-15">{MenuData.categories[2].name}</h2>
-            <p className="sb-text" dangerouslySetInnerHTML={{__html : MenuData.categories[2].description}} />
+            <h2 className="sb-cate-title sb-mb-15">
+              {MenuData.categories[2].name}
+            </h2>
+            <p
+              className="sb-text"
+              dangerouslySetInnerHTML={{
+                __html: MenuData.categories[2].description,
+              }}
+            />
           </div>
 
-          <MenuGrid items={MenuData.categories[2].items} />
+          {/* <MenuGrid items={MenuData.categories[2].items} /> */}
         </div>
       </section>
       {/* menu section 3 end */}
@@ -71,11 +92,18 @@ const Menu1 = () => {
       <section className="sb-menu-section sb-p-0-60">
         <div className="container">
           <div className="sb-mb-60">
-            <h2 className="sb-cate-title sb-mb-15">{MenuData.categories[3].name}</h2>
-            <p className="sb-text" dangerouslySetInnerHTML={{__html : MenuData.categories[3].description}} />
+            <h2 className="sb-cate-title sb-mb-15">
+              {MenuData.categories[3].name}
+            </h2>
+            <p
+              className="sb-text"
+              dangerouslySetInnerHTML={{
+                __html: MenuData.categories[3].description,
+              }}
+            />
           </div>
 
-          <MenuGrid items={MenuData.categories[3].items} />
+          {/* <MenuGrid items={MenuData.categories[3].items} /> */}
         </div>
       </section>
       {/* menu section 4 end */}
